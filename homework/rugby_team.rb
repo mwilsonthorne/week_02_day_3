887660497c2fc9ec4d67c12b2349831898b41763
Matthew Thorne <mwilsonthorne@gmail.com>
2018-03-19 09:17:05 +0000

    Completed add/remove rugby player to squad

--- homework/rugby_team.rb
+++ homework/rugby_team.rb
@@ -12,4 +12,13 @@ def playing_squad_available()
   return @squad.count()
 end
 
+
+def add_a_player_to_playing_squad(player)
+  @squad.push(player)
+end
+
+def remove_a_player_from_playing_squad(player)
+  @squad.delete(player)
+end
+
 end
--- homework/specs/rugby_team_spec.rb
+++ homework/specs/rugby_team_spec.rb
@@ -5,12 +5,13 @@ require_relative("../rugby_player.rb")
 class RugbyTeamTest < Minitest::Test
 
 def setup
-  player1 = RugbyPlayer.new("Greg", 32, 85)
+  @player1 = RugbyPlayer.new("Greg", 32, 85)
   player2 = RugbyPlayer.new("Huw", 23, 100)
   player3 = RugbyPlayer.new("John", 31, 110)
   player4 = RugbyPlayer.new("Stuart", 25, 95)
-  playing_squad = [player1, player2, player3, player4]
+  playing_squad = [@player1, player2, player3, player4]
   @rugby_team = RugbyTeam.new("Scotland", playing_squad)
+  @player = RugbyPlayer.new("Tommy", 29, 95)
 end
 
 def test_rugby_team_name
@@ -21,8 +22,13 @@ def test_playing_squad_available
   assert_equal(Array, @rugby_team.squad.class)
 end
 
+def test_add_a_player_to_playing_squad()
+  @rugby_team.add_a_player_to_playing_squad(@player1)
+  assert_equal(5, @rugby_team.squad.length)
+end
 
-
-
-
+def test_remove_a_player_from_playing_squad()
+  @rugby_team.remove_a_player_from_playing_squad(@player1)
+  assert_equal(3, @rugby_team.squad.count)
+end 
 end
